d815c7751550ebe4b89c7d434705847d53cb3807
0younge <s45628439@gmail.com>
2026-05-18 21:05:45 +0900

    GitHub Actions #9

--- src/test/java/com/example/_younge/ApplicationTests.java
+++ src/test/java/com/example/_younge/ApplicationTests.java
@@ -2,8 +2,10 @@ package com.example._younge;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class ApplicationTests {
 
     @Test
